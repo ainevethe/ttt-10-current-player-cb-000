5401265ad15af45916f9c3ff04d6370cb10c7ce5
Crista Cooper <aine3d@gmail.com>
2017-11-04 19:12:09 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,16 @@
 
-def turn_count
-
-
-
+def turn_count(board)
+  counter = 0
+  board.each do | place |
+    if place == "X" || "O"
+    counter += 1
+    end
+  end
 end
 
 
 def current_player
 
-
+  
 
 end
